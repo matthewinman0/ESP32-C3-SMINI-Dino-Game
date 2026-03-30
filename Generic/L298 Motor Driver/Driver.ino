05c8070b65b8bbcec66c1aecf28ae13fbc542efd
LucasWh-design <elchine69@gmail.com>
2026-03-30 21:46:06 +0100

    Update Driver.ino

--- Generic/L298 Motor Driver/Driver.ino
+++ Generic/L298 Motor Driver/Driver.ino
@@ -10,13 +10,49 @@ int in4 = 27;
 
 //PWM config
 const int freq = 2000;
-const int pwmchaneA = 0;
-const int pwmchaneB = 1;
+const int pwmchanelA = 0;
+const int pwmchanelB = 1;
 const int resoloution = 8;
 
 void setup(){
-pinmode(in1, OUTPUT);
+
+pinmode(in1, OUTPUT);//set all motor pins as outputs
 pinmode(in2, OUTPUT);
 pinmode(in3, OUTPUT);
 pinmode(in4, OUTPUT);
 
+digitalWrite(in1, LOW);//Set motors off
+digitalWrite(in2, LOW);
+digitalWrite(in3, LOW);
+digitalWrite(in4, LOW);
+
+LedcAttachChannel(ENA, freq, resoloution, pwmchanelA);
+LedcAttachChannel(ENB, freq, resoloution, pwmchanelB);
+
+
+LedcWrite(ENA, 0);//Set motor speed to 0
+LedcWrite(ENB, 0);//Set motor speed to 0
+
+}
+
+void loop(){
+
+//example code
+
+LedcWrite(ENA, 255);//Motors full speed ahead!
+LedcWrite(ENB, 255);
+
+//Set motors one direction ( if you want to see how motor directions work see README )
+digitalWrite(in1, HIGH);
+digitalWrite(in2, LOW);
+digitalWrite(in3, HIGH);
+digitalWrite(in4, LOW);
+
+//Reverse motors
+
+digitalWrite(in1, LOW);
+digitalWrite(in2, HIGH);
+digitalWrite(in3, LOW);
+digitalWrite(in4, HIGH);
+
+}
